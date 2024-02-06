92b76257a05ffc5cc129a0ceb0217b3533b80ee0
SazukeR <reinaldojcg1@gmail.com>
2024-02-06 09:56:02 -0500

    crud-nest, custom pipe to delete data

--- src/pokemon/pokemon.service.ts
+++ src/pokemon/pokemon.service.ts
@@ -1,8 +1,8 @@
-import { BadRequestException, Injectable, InternalServerErrorException } from '@nestjs/common';
+import { BadRequestException, Injectable, InternalServerErrorException, NotFoundException } from '@nestjs/common';
 import { CreatePokemonDto } from './dto/create-pokemon.dto';
 import { UpdatePokemonDto } from './dto/update-pokemon.dto';
 import { Pokemon } from './entities/pokemon.entity';
-import { Model } from 'mongoose';
+import { Model, isValidObjectId } from 'mongoose';
 import { InjectModel } from '@nestjs/mongoose';
 
 @Injectable()
@@ -28,15 +28,7 @@ export class PokemonService {
 
     } catch (error) {
       
-      // 11000 es el codigo de error que se lanzaria si ya hubiera un registro creado con ese nombre de pokemon o con ese no de pokemon (recordando que nosotros validamos las entitys para preveer que hayan llaves duplicadas)
-
-      // de esta forma manejamos los errores para poder enviarselos a front-end, siendo mas especificos en lo que causo el problema
-      if (error.code === 11000) {
-        throw new BadRequestException(`Pokemon exist in db ${JSON.stringify( error.keyValue )}`);
-      }
-
-
-      throw new InternalServerErrorException('Cant create Pokemon - check server logs')
+      this.handleExceptions( error )
 
     }
 
@@ -46,15 +38,76 @@ export class PokemonService {
     return `This action returns all pokemon`;
   }
 
-  findOne(id: number) {
-    return `This action returns a #${id} pokemon`;
+  async findOne(term: string) {
+
+    let pokemon: Pokemon;
+
+    if ( !isNaN(+term) ) {
+      pokemon = await this.pokemonModel.findOne({ no: term });
+    }
+
+    if ( !pokemon && isValidObjectId(term) ) {
+      pokemon = await this.pokemonModel.findById(term);
+    }
+
+    if ( !pokemon ) {
+      pokemon = await this.pokemonModel.findOne({name: term});
+    }
+
+
+    if( !pokemon ) {
+      throw new NotFoundException(`Pokemon with id, name or no. ${term} not found`);
+    }
+
+    return pokemon;
   }
 
-  update(id: number, updatePokemonDto: UpdatePokemonDto) {
-    return `This action updates a #${id} pokemon`;
+  async update(term: string, updatePokemonDto: UpdatePokemonDto) {
+
+    const pokemon = await this.findOne(term);
+
+    if ( updatePokemonDto.name ) {
+      updatePokemonDto.name = updatePokemonDto.name.toLocaleLowerCase();
+    }
+
+    try {
+      
+      await pokemon.updateOne(updatePokemonDto);
+
+      return { ...pokemon.toJSON(), ...updatePokemonDto };
+
+
+    } catch (error) {
+
+      this.handleExceptions( error )
+
+    }
+
   }
 
-  remove(id: number) {
-    return `This action removes a #${id} pokemon`;
+  async remove(id: string) {
+
+    const result = await this.pokemonModel.findByIdAndDelete( id );
+
+
+    return result;
+    
+/*     const pokemon = await this.findOne(id);
+    await pokemon.deleteOne(); */
+
+  }
+
+  // nos creamos una funcion para amnejar los errores en el update y create, es de tipo any para que pueda manejar cualquier tipo de error
+  private handleExceptions( error: any ) {
+    // 11000 es el codigo de error que se lanzaria si ya hubiera un registro creado con ese nombre de pokemon o con ese no de pokemon (recordando que nosotros validamos las entitys para preveer que hayan llaves duplicadas)
+
+    // de esta forma manejamos los errores para poder enviarselos a front-end, siendo mas especificos en lo que causo el problema
+    
+    if (error.code === 11000) {
+      throw new BadRequestException(`Pokemon exist in db ${JSON.stringify( error.keyValue )}`);
+    }
+
+    throw new InternalServerErrorException('Cant create Pokemon - check server logs');
+
   }
 }
